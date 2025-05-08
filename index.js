09529ad3fccb0b44e931f876a215f65af28ab868
fisker <lionkay@gmail.com>
2025-05-08 09:35:37 +0800

    Iterate from `process.cwd()` by default

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as path from 'node:path'
+import process from 'node:process'
 import {toAbsolutePath} from 'url-or-path'
 
 /** @import {UrlOrPath, OptionalUrlOrPath} from 'url-or-path' */
@@ -6,12 +7,12 @@ import {toAbsolutePath} from 'url-or-path'
 /**
  * Yields paths between `from` and `to`.
  *
- * @param {UrlOrPath} from
+ * @param {OptionalUrlOrPath} [from]
  * @param {OptionalUrlOrPath} [to]
  * @returns {Generator<string>}
  */
 function* iterateDirectoryUp(from, to) {
-  from = toAbsolutePath(from)
+  from = toAbsolutePath(from) ?? process.cwd()
   to = to ? toAbsolutePath(to) : path.parse(from).root
 
   // `from` is not a child directory of `to`
