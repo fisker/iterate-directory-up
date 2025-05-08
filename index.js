cc20d15d751fdb71d2868f0a6d8748d31bdbcdac
fisker <lionkay@gmail.com>
2025-05-08 09:43:28 +0800

    Add example

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ import {toAbsolutePath} from 'url-or-path'
 /** @import {UrlOrPath, OptionalUrlOrPath} from 'url-or-path' */
 
 /**
- * Yields paths between `from` and `to`.
- *
- * @param {OptionalUrlOrPath} [from]
- * @param {OptionalUrlOrPath} [to]
- * @returns {Generator<string>}
- */
+Yields paths between `from` and `to`.
+
+@param {OptionalUrlOrPath} [from] - The directory iteration starts.
+@param {OptionalUrlOrPath} [to] - The directory iteration stops.
+@returns {Generator<string>}
+
+@example
+```js
+import iterateDirectoryUp from 'iterate-directory-up'
+
+for (const directory of iterateDirectoryUp('/a/b')) {
+  console.log(directory)
+}
+// "/a/b"
+// "/a"
+// "/"
+```
+*/
 function* iterateDirectoryUp(from, to) {
   from = toAbsolutePath(from) ?? process.cwd()
   to = to ? toAbsolutePath(to) : path.parse(from).root
